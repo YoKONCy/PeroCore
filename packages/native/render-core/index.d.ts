420f1267331b1bb333539ca00d28f6d4e3c984de
YoKONCy <2752026184@qq.com>
2026-04-21 20:14:32 +0800

    chore: 将 render-core 注册为 git submodule (私有源码隔离)

--- packages/native/render-core/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-
-export declare function encrypt(data: Buffer, key: string): Buffer
-
-export declare function decrypt(data: Buffer, key: string): Buffer
-
-export declare function verifyIntegrity(data: Buffer, hash: string): boolean
